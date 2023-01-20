8a46ab61e47269e7698f5ce83055d8ca70b718ac
joaquinizquierdo <izquierdo.joaco@gmail.com>
2023-01-19 21:30:38 -0300

    Entrega final

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ usuario = localStorage.getItem('nombre');
 console.log(usuario);
 
 let nombre = document.getElementById('nombreOperador');
+nombre.innerText = 'Operador ' + usuario;
 
 if (usuario == '') {
     usuario = prompt('Ingresar nombre del operador');
     localStorage.setItem('nombre', usuario);
-    nombre.innerText = 'Operador' + usuario;
+    nombre.innerText = 'Operador ' + usuario;
 }
 
 const limpiar = document.getElementById ("btnBorrar");
@@ -61,6 +62,14 @@ function guardarReserva () {
 
     const horarios = document.getElementById ('horariosCancha');
     horarios.innerText = 'Horarios: ' + cancha1.hora.join(', ');
+
+    Toastify ({
+        text: "Reserva guardada",
+        duration: 1200,
+        style: {
+            background: '#515151'
+        }
+    }).showToast();
 }
 
 //Boton de descuento
@@ -72,11 +81,51 @@ descuento.addEventListener ("click", aplicarDescuento);
 function aplicarDescuento () {
     if (descuentoDato === false) {
         descuentoDato = true;
-        console.log('Descuento aplicado');
         descuento.innerText = 'Cancelar descuento';
+        Toastify ({
+            text: "Descuento aplicado",
+            duration: 1000,
+            style: {
+                background: '#515151'
+            }
+        }).showToast();
     } else {
         descuentoDato = false;
-        console.log('Descuento removido');
         descuento.innerText = 'Aplicar descuento';
+        Toastify ({
+            text: "Descuento removido",
+            duration: 1000,
+            style: {
+                background: '#515151'
+            }
+        }).showToast();
     }
 }
+
+//Clima
+
+const getWeatherByCity = async () => {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=buenos aires&appid=e90599a9e72a1d9d12374c927799735b`)
+    const data = await response.json()
+    showWeather(data)
+};
+
+getWeatherByCity()
+
+const showWeather = (data) => {
+const temp = getCelsius(data.main.temp)
+const div = document.createElement('div')
+
+div.innerHTML = `
+    <h4>
+        ${data.name}: ${temp}°, ${data.weather[0].main}
+    </h4>
+
+`
+clima.innerHTML = ''
+clima.appendChild(div)
+};
+
+const getCelsius = (kelvin) => {
+return Math.floor(kelvin - 273)
+};
