8c20fc55cea0d3c248f767739fcd6fa026e41d33
joaquinizquierdo <izquierdo.joaco@gmail.com>
2023-02-04 16:09:09 -0300

    correccion tp js

--- main.js
+++ main.js
@@ -19,7 +19,39 @@ function limpiarStorage () {
     usuario = prompt('Ingresar nombre del operador');
     localStorage.setItem('nombre', usuario);
     nombre.innerText = 'Operador ' + usuario;
+    localStorage.clear()
 }
+
+let guardado;
+
+guardado = document.getElementById("btnGuardado");
+guardado.addEventListener("click", obtenerDatos);
+
+function obtenerDatos () {
+    let storageHoras = [];
+    storageHoras = localStorage.getItem('horariosGuardados').split(",");
+
+    let storageRecaudado;
+    storageRecaudado = localStorage.getItem('recaudadoGuardado');
+
+    let storageVeces;
+    storageVeces = localStorage.getItem('vecesGuardado');
+
+    const vecesPrevia = document.getElementById ('vecesPrevia');
+    vecesPrevia.innerText = 'Veces usada: ' + storageVeces;
+
+    const totalPrevia = document.getElementById ('totalPrevia');
+    totalPrevia.innerText = 'Recaudado: $' + storageRecaudado;
+
+    storageHoras.forEach( (horas) => {
+        const p = document.createElement('p')
+        p.classList.add('tarjeta')
+        p.innerHTML = `
+            ${horas}
+        `
+        horasPrevia.appendChild(p)
+        });
+    }
 
 class Cancha {
     constructor (precio) {
@@ -34,13 +66,25 @@ class Cancha {
         this.vecesUsada++;
         if (descuentoDato === true) {
             this.recaudado = this.recaudado + this.valor[1];
-            descuentoDato = false; //Reseteo el valor
-            descuento.innerText = 'Aplicar descuento'; //Reseteo el boton
+            descuentoDato = false; //Reset del valor para no aplicar el descuento
+            descuento.innerText = 'Aplicar descuento'; //Reset del boton de descuento
         } else {
             this.recaudado = this.recaudado + this.valor[0];
         }
         this.hora.push(this.horaDato.value);
         this.hora.sort((a,b) => a - b);
+        localStorage.setItem('horariosGuardados', this.hora);
+        localStorage.setItem('recaudadoGuardado', this.recaudado);
+        localStorage.setItem('vecesGuardado', this.vecesUsada);
+    }
+    tarjeta () {
+        const p = document.createElement('p')
+        p.classList.add('tarjeta')
+
+        p.innerHTML = `
+            ${this.horaDato.value}
+        `
+        horas.appendChild(p)
     }
 }
 
@@ -53,15 +97,13 @@ cancha1.horaDato = document.getElementById("horario");
 
 function guardarReserva () {
     cancha1.guardarValores ();
+    cancha1.tarjeta ();
 
     const veces = document.getElementById ('vecesCancha');
     veces.innerText = 'Veces usada: ' + cancha1.vecesUsada;
 
     const total = document.getElementById ('totalCancha');
     total.innerText = 'Recaudado: $' + cancha1.recaudado;
-
-    const horarios = document.getElementById ('horariosCancha');
-    horarios.innerText = 'Horarios: ' + cancha1.hora.join(', ');
 
     Toastify ({
         text: "Reserva guardada",
@@ -72,7 +114,7 @@ function guardarReserva () {
     }).showToast();
 }
 
-//Boton de descuento
+//Boton para la aplicacion del descuento
 
 let descuentoDato = false;
 const descuento = document.getElementById ("btnDescuento");
@@ -81,7 +123,7 @@ descuento.addEventListener ("click", aplicarDescuento);
 function aplicarDescuento () {
     if (descuentoDato === false) {
         descuentoDato = true;
-        descuento.innerText = 'Cancelar descuento';
+        descuento.innerText = 'Cancelar';
         Toastify ({
             text: "Descuento aplicado",
             duration: 1000,
@@ -102,7 +144,7 @@ function aplicarDescuento () {
     }
 }
 
-//Clima
+//Funcion para recibir la temperatura del clima
 
 const getWeatherByCity = async () => {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=buenos aires&appid=e90599a9e72a1d9d12374c927799735b`)
@@ -113,19 +155,18 @@ const getWeatherByCity = async () => {
 getWeatherByCity()
 
 const showWeather = (data) => {
-const temp = getCelsius(data.main.temp)
-const div = document.createElement('div')
+    const temp = getCelsius(data.main.temp)
+    const div = document.createElement('div')
 
-div.innerHTML = `
-    <h4>
-        ${data.name}: ${temp}°, ${data.weather[0].main}
-    </h4>
-
-`
-clima.innerHTML = ''
-clima.appendChild(div)
+    div.innerHTML = `
+        <h4>
+            ${data.name}: ${temp}°, ${data.weather[0].main}
+        </h4>
+    `
+    clima.innerHTML = ''
+    clima.appendChild(div)
 };
 
 const getCelsius = (kelvin) => {
-return Math.floor(kelvin - 273)
+    return Math.floor(kelvin - 273)
 };
